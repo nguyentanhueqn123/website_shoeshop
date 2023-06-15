527f8cb7f6c1dfe31b1f4d8222658a8127aa73f1
Huệ Nguyễn Tấn <20521359@ms.uit.edu.vn>
2023-06-16 02:04:54 +0700

    done list infor order user

--- website_shoe/src/views/Order/OrderBox.js
+++ website_shoe/src/views/Order/OrderBox.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useFetchProducts, useProducts, useFetchAllProductType} from '../../store/product/hook'
 import { formatDDMMYYYYHHmm } from '../../utils/formatDatetime'
 import Price from '../../components/Price/Price'
+import "./OrderBox.scss"
 
 
 
@@ -18,67 +19,92 @@ export default function OrderBox({invoice}) {
   useFetchAllProductType()
 
     return (
-        <div className="flex border bg-white rounded-lg p-4 mb-4">
+        <div className="flex border bg-white rounded-lg p-6 mb-4">
             <div className="w-2/5">
                 <p>
-                {invoice.product.map((productId, index) => {
-                    const product = products?.data.find(p => p._id === productId);
-                    if (product) {
-                      return <a href={`/san-pham/${product._id}`} className="flex items-center mb-2" key={index}>
-                        <img className="w-[70px] h-[70px] border object-cover rounded-md shadow-sm" src={product.image} alt="imageProduct" />
-                        <div className="ml-2">
-                            <p className="">{product.nameProduct}</p>
-                            <Price
-                                price={product?.priceSale}
-                                priceDel={product?.price}
-                                color="black"
-                                className="text-[1rem]"
-                            />
+                    {invoice.product.map((productId, index) => {
+                        const product = products?.data.find((p) => p._id === productId);
+                        if (product) {
+                            // Check if the current product ID is the same as the previous one
+                            if (index > 0 && productId === invoice.product[index - 1]) {
+                            return null; // If so, return null to skip rendering this product
+                            }
+
+                            // Get the quantity of this product
+                            const quantity = invoice.product.filter((id) => id === productId).length;
+
+                            return (
+                            <a href={`/san-pham/${product._id}`} className="flex items-center mb-3" key={index}>
+                                <div className="w-full flex justify-between items-center container-box">
+                                    <div className="flex">
+                                        <img className="w-[70px] h-[70px] border object-cover rounded-md shadow-sm" src={product.image} alt="imageProduct" />
+                                        <div className="ml-2">
+                                            <p className="mt-[-2px]">{product.nameProduct}</p>
+                                            <Price price={product?.priceSale} priceDel={product?.price} color="black" className="text-[1rem]" />
+                                            <span className="ml-1 text-gray-500">x {quantity}</span> {/* Display the quantity */}
+                                        </div>
+                                    </div>
+                                    <button className="bg-[#62B4FF] h-10 rounded-lg px-3 text-white opacity-0 hover:bg-[#349eff] btn-buy">Buy Again</button>
+                                </div>
+                            </a>
+                            );
+                        }
+                    })}
+                </p>
+            </div>
+            <div className="w-3/5 ml-3 flex px-6 py-2 rounded-lg border shadow-md overflow-hidden">
+                <div className="w-1/2">
+                    <p className="w-full py-3 border-b-2 uppercase text-[#62B4FF] font-bold">Desciption</p>
+                    <div className="">
+                        <p className="mt-4">
+                            <span className="text-gray-500 mr-1">ID order:</span>
+                            {invoice?._id.slice(0, 4)}...{invoice?._id?.slice(invoice._id?.length - 4, invoice?._id?.length)}
+                        </p>
+                        <p className="mt-1">
+                            <span className="text-gray-500">Total Quanlity:</span>
+                            <span className="ml-1">{invoice?.amount}</span>
+                        </p>
+                        <p className="mt-1 text-gray-500">
+                            <span className="">Amount due:</span>
+                            <span className="ml-1 font-bold">{invoice?.cost} đ</span>
+                        </p>
+                        <p className="mt-4">
+                            <i className="fa-solid fa-calendar-days mr-2 text-[#62B4FF]"></i>
+                            Time Order:
+                            <span className="ml-1">{formatDDMMYYYYHHmm(invoice?.time)}</span>
+                        </p>
+                        <p className="mt-1 mb-2">
+                            <i className="fa-solid fa-credit-card mr-2 text-[#62B4FF]"></i>
+                            Payment Method:
+                            <span className="ml-1">{invoice?.paymentMethod}</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="w-1/2 ml-3">
+                    <p className="w-full px-6 py-3 border-b-2 uppercase text-[#62B4FF] font-bold">Delivery Address</p>
+
+                    <div className="px-6 py-5">
+                        {
+                            userLogin?.nameAccount
+                        }
+                        <div className="flex items-center mt-1">
+                            <i className="fa-solid fa-mobile-screen-button text-[#62B4FF]"></i>
+                            <p className="ml-2 text-gray-500">
+                            {invoice?.phone}
+                            </p>
                         </div>
-                      </a>
-                    }
-                })}
-                </p>
-            </div>
-            <div className="w-2/5">
-                <p>ID: {invoice?._id}</p>
-                <p>
-                Total Quanlity: 
-                {invoice?.amount}
-                </p>
-                <p>
-                <i className="fa-solid fa-tag"></i>
-                Total Price:
-                {invoice?.cost}
-                </p>
-                <p>
-                Time Order:
-                {formatDDMMYYYYHHmm(invoice?.time)}
-                </p>
-                <p>
-                Payment Method:
-                {invoice?.paymentMethod}
-                </p>
-            </div>
-            <div className="w-1/5 border-l-2 pl-3">
-                <p className="uppercase font-bold mb-2 text-[#62B4FF]">Delivery Address</p>
-                {
-                    userLogin?.nameAccount
-                }
-                <div className="flex items-center mt-1">
-                    <i className="fa-solid fa-mobile-screen-button text-[#62B4FF]"></i>
-                    <p className="ml-2 text-gray-500">
-                    {invoice?.phone}
-                    </p>
+                        
+                        <div className="flex items-center mt-1">
+                            <i className="fa-sharp fa-solid fa-location-dot text-[#62B4FF]"></i>
+                            <p className="ml-2 text-gray-500">
+                            {invoice?.address}
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 
-                <div className="flex items-center mt-1">
-                    <i className="fa-sharp fa-solid fa-location-dot text-[#62B4FF]"></i>
-                    <p className="ml-2 text-gray-500">
-                    {invoice?.address}
-                    </p>
-                </div>
             </div>
+            
             
         </div>
     )
